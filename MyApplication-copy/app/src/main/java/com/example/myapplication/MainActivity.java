f96251af4e9ff50eb3700c43a6c906697a316bb8
Lihua-WANG <lihuwang@student.unimelb.edu.au>
2021-10-12 18:33:50 +1100

    read the csv file and get the data

--- MyApplication-copy/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication-copy/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,15 +17,38 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
     private  LineChart lineChart;
-
+    private String item;
+    private int quantity;
+    private int x = 0;
+    List<TestDataBean> user1_recipt1 = new ArrayList<>();
+    List<TestDataBean> user1_shoppinglist1 = new ArrayList<>();
+    List<TestDataBean> user1_recipt2 = new ArrayList<>();
+    List<TestDataBean> user1_shoppinglist2 = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // read the data
+        user1_recipt1 = readCsv(this.getFilesDir().getAbsolutePath() + File.separator + "user1_recipt" + ".csv");
+        user1_shoppinglist1 = readCsv(this.getFilesDir().getAbsolutePath() + File.separator + "user1_shoppingList" + ".csv");
+        user1_recipt2 = readCsv(this.getFilesDir().getAbsolutePath() + File.separator + "user1_recipt_2" + ".csv");
+        user1_shoppinglist2 = readCsv(this.getFilesDir().getAbsolutePath() + File.separator + "user1_shoppingList_2" + ".csv");
+
+        for (int i = 0; i < user1_recipt1.size(); i++){
+            x = user1_recipt1.get(i).getQuantity();
+            x+=x;
+        }
+        System.out.print(x);
 
         lineChart = (LineChart) findViewById(R.id.linechart);
 
@@ -63,5 +86,28 @@ public class MainActivity extends AppCompatActivity {
         xAxis.setGranularityEnabled(true);
         xAxis.setValueFormatter(new IndexAxisValueFormatter(xLabel));
 
+    }
+
+    private ArrayList<TestDataBean> readCsv(String path) {
+        ArrayList<TestDataBean> readerArr = new ArrayList<>();
+        File file = new File(path);
+        FileInputStream fileInputStream;
+        Scanner in;
+        try {
+            fileInputStream = new FileInputStream(file);
+            in = new Scanner(fileInputStream, "UTF-8");
+            in.nextLine();
+            while (in.hasNextLine()) {
+                String[] lines = in.nextLine().split(",");
+                item = lines[0];
+                quantity = Integer.parseInt(lines[1]);
+                TestDataBean bean = new TestDataBean(item, quantity);
+                readerArr.add(bean);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return readerArr;
     }
 }
