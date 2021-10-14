60cfc3a2f9d200df3d2c465b9493be48a0bf611a
puyang1996 <puyang@student.unimelb.edu.au>
2021-10-14 18:45:27 +1100

    update leaderboard
    update leaderboard

---
+++ MyApplication-test/app/src/main/java/com/example/myapplication/FriendRankFragment.java
@@ -0,0 +1,97 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FriendRankFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FriendRankFragment extends Fragment {
+
+
+    Button bt_change_public;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FriendRankFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FriendRankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FriendRankFragment newInstance(String param1, String param2) {
+        FriendRankFragment fragment = new FriendRankFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friend_rank, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+
+
+
+
+
+        bt_change_public = getActivity().findViewById(R.id.button_change_public);
+        bt_change_public.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                getActivity().setContentView(R.layout.fragment_rank);
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+}
--- MyApplication-test/app/src/main/java/com/example/myapplication/RankFragment.java
+++ MyApplication-test/app/src/main/java/com/example/myapplication/RankFragment.java
@@ -1,29 +1,109 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link RankFragment#newInstance} factory method to
+ * Use the {@link RankFragment#} factory method to
  * create an instance of this fragment.
  */
 public class RankFragment extends Fragment {
 
+    private FragmentManager manager;
+    private FragmentTransaction ft;
+    private ImageView bt;
+    private TextView tv;
+    private int  xx=0;
+    Button bt_share;
+    Button bt_change_friends;
+    Button bt_change_public;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        manager = getFragmentManager();
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rank, container, false);
+    }
+
+
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+        bt = getActivity().findViewById(R.id.heart1);
+        tv = getActivity().findViewById(R.id.number1);
+        bt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+
+                String text = tv.getText().toString()+"";
+                int number = Integer.valueOf(text);
+                number++;
+                tv.setText(""+number);
+//                tv.setText(Integer.valueOf(tv.getText().toString())+1);
+  //                tv.setText(""+xx++);
+//                if (xx==10){
+//                    Toast.makeText(MainActivity.this, "牛逼", Toast.LENGTH_SHORT).show();
+//                }
+            }
+        });
+
+        bt_share = getActivity().findViewById(R.id.button_share);
+        bt_share.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String body = "Your body here";
+                String sub = "Your Subject";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT,sub);
+                myIntent.putExtra(Intent.EXTRA_TEXT,body);
+                startActivity(Intent.createChooser(myIntent, "Share Using"));
+            }
+        });
+
+
+
+        bt_change_friends = getActivity().findViewById(R.id.button_change_friends);
+        bt_change_friends.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                getActivity().setContentView(R.layout.fragment_friend_rank);
+            }
+        });
+
+
+
+
+
     }
 }
