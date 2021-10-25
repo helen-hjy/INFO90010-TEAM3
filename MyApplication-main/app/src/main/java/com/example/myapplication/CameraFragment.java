87575b836534e7fa1caa67837a5c83a11f13c25d
Helen Han <jiayu.han@student.unimelb.edu.au>
2021-10-25 16:04:05 +1100

    update folder name

--- MyApplication-main/app/src/main/java/com/example/myapplication/CameraFragment.java
+++ MyApplication-main/app/src/main/java/com/example/myapplication/CameraFragment.java
@@ -8,11 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CameraFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CameraFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
