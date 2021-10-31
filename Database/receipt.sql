3b8593bbfc94c08a0ac6faaf66c6dab9107e5a3f
Lihua-WANG <lihuwang@student.unimelb.edu.au>
2021-10-31 14:29:39 +1100

    Database file

---
+++ Database/receipt.sql
@@ -0,0 +1,12 @@
+-- TABLE
+CREATE TABLE receipt (Receipt_id integer, Date varchar(20), Item_id text primary key, Item_quantity INTEGER );
+CREATE TABLE receipt2 (Receipt_id integer, Date varchar(20), Item_id text primary key, Item_quantity INTEGER );
+CREATE TABLE shoppingList (Shopping_list_id integer, Date varchar(20), Item_id text primary key, Item_quantity INTEGER );
+CREATE TABLE shoppingList2 (Shopping_list_id integer, Date varchar(20), Item_id text primary key, Item_quantity INTEGER );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
